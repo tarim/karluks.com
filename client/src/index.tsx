938789e6e24a31195f8dfd599c7441644a2ca442
Nur Karluk <ns.karluk@gmail.com>
2018-09-15 23:49:47 -0400

    added react-router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,16 +2,17 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import createStore from './store';
-import App from './App';
+import Routes from './components/router';
 import registerServiceWorker from './registerServiceWorker';
 import ReduxToastr from 'react-redux-toastr';
+import { Router, BrowserRouter } from 'react-router-dom';
 let store=createStore({});
 ReactDOM.render(
   <Provider store={store}>
       <div>
         <ReduxToastr timeOut={30000} newestOnTop={false} preventDuplicates position='top-right' transitionIn='fadeIn'
           transitionOut='fadeOut' progressBar />
-        <App />
+        <Router history={BrowserRouter} routes={Routes} />
       </div>
     </Provider>
   ,

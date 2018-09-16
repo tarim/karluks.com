3b27dff0c27f73d22fefbafa42691dd76863017a
Nur Karluk <ns.karluk@gmail.com>
2018-09-15 23:07:07 -0400

    connected to redux store added redux-form added react-redux-toastr for message

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,20 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import createStore from './store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
+import ReduxToastr from 'react-redux-toastr';
+let store=createStore({});
 ReactDOM.render(
-  <App />,
+  <Provider store={store}>
+      <div>
+        <ReduxToastr timeOut={30000} newestOnTop={false} preventDuplicates position='top-right' transitionIn='fadeIn'
+          transitionOut='fadeOut' progressBar />
+        <App />
+      </div>
+    </Provider>
+  ,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
